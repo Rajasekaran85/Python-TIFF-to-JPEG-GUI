a37b097476bcb249e8e6376a5c2dccd114eaed62
Rajasekaran A <rajasekaran.al@gmail.com>
2022-11-09 17:04:11 +0530

    Update tiff-to-jpeg_GUI.py
    All Resolution Images can convert into JPEG format

--- tiff-to-jpeg_GUI.py
+++ tiff-to-jpeg_GUI.py
@@ -20,8 +20,7 @@ def browse_button():
 
     
 def conv():
-    print("\n TIFF to JPG Conversion - 300 DPI \n")
-    print("\n Developed by A Rajasekaran\n")
+    print("\n TIFF to JPG Conversion\n")
     print("\n Date: 22 April 2022 \n\n")
 
     directory = "JPEG"
@@ -38,7 +37,7 @@ def conv():
         test = os.path.splitext(fname)[0]
         value1 = filename + '/' + fname
 
-	# open the tif image
+        # open the tif image
         image = Image.open(value1)
 
         # define jpg file name as same tif image name
@@ -56,20 +55,22 @@ def conv():
     messagebox.showinfo("TIFF to JPG", "Completed")
     
 root = Tk()
-root.geometry("400x250")
+root.geometry("400x350")
+root.title("TIFF to JPEG Converter")
 root.config(bg='#ffcc00') 
 
+img = "logo.png"
+logo = PhotoImage(file=img)
+label1 = Label(root, image = logo) 
+label1.pack(pady=20)
+
 folder_path = StringVar()
-label_value = 'TIFF to JPG Conversion - 300 DPI'
-lbl1 = Label(root, text='TIFF to JPG Conversion - 300 DPI', font='helvetica 15', bg='#ffcc00')
+lbl1 = Label(root, text='TIFF to JPEG Converter', font='helvetica 15', bg='#ffcc00')
 lbl1.pack(pady=10)
 button2 = Button(text="Browse", command=browse_button, bg='royalblue', fg = 'white').pack(pady=25)
 button3 = Button(text="Submit", command=conv, bg='royalblue', fg = 'white').pack(pady=25)
 lbl2 = Label(root, textvariable=folder_path, font='helvetica 12', bg='#ffcc00')
 lbl2.pack(pady=10)
 
-
-
 mainloop()
-
 
